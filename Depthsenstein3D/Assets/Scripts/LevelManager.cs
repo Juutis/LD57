24d0c270621531a3a1f5572b4d581b54b1cd0345
Matias Pitkänen <M2tias@Gmail.com>
2025-04-07 16:26:13 +0300

    puishs

--- Depthsenstein3D/Assets/Scripts/LevelManager.cs
+++ Depthsenstein3D/Assets/Scripts/LevelManager.cs
@@ -92,6 +92,7 @@ public class LevelManager : MonoBehaviour
         FpsManager.Main.FreezeControls();
         MusicManager.main.SwitchMusic(MusicType.Elevator);
         ElevatorDoors currentElevator = currentLevel.GetComponentInChildren<ElevatorDoors>();
+        MapGenerator.main.ResetKeys();
         MapGenerator.main.Player.ElevatorRotate(currentElevator.transform.position, delegate
         {
             LevelStats stats = MapGenerator.main.Player.Stats.CalculateCurrentLevelStats();
